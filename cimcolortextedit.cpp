9f0aed5b742c83aa7b9cf4ee464fc9ff93dbf1cf
Alfred Reinold Baudisch <alfred@alfredbaudisch.com>
2024-04-11 20:22:25 +0100

    Initial release, with Render and SetText exposed and initial instructions

---
+++ cimcolortextedit.cpp
@@ -0,0 +1,34 @@
+﻿#include "imgui.h"
+#include "imgui_internal.h"
+#include "ImGuiColorTextEdit/TextEditor.h"
+#include "cimcolortextedit.h"
+
+CIMGUI_API TextEditor* ImColorTextEdit_TextEditor(void)
+{
+    return IM_NEW(TextEditor)();
+}
+
+CIMGUI_API void ImColorTextEdit_destroy(TextEditor* self)
+{
+    IM_DELETE(self);
+}
+
+CIMGUI_API void ImColorTextEdit_TextEditor_SetText(TextEditor* self, const char *text)
+{
+    self->SetText(text);
+}
+
+CIMGUI_API void ImColorTextEdit_TextEditor_Render(TextEditor* self, const char* aTitle)
+{
+    self->Render(aTitle);
+}
+
+CIMGUI_API void ImColorTextEdit_TextEditor_RenderSize(TextEditor* self, const char* aTitle, const ImVec2* aSize)
+{
+    self->Render(aTitle, *aSize);
+}
+
+CIMGUI_API void ImColorTextEdit_TextEditor_RenderSizeBorder(TextEditor* self, const char* aTitle, const ImVec2* aSize, bool aBorder)
+{
+    self->Render(aTitle, *aSize, aBorder);
+}
---
+++ cimcolortextedit.h
@@ -0,0 +1,17 @@
+﻿#ifndef CIMGUI_COLOR_TEXT_EDIT_INCLUDED
+#define CIMGUI_COLOR_TEXT_EDIT_INCLUDED
+
+#include "cimgui.h"
+
+typedef struct TextEditor TextEditor;
+
+CIMGUI_API TextEditor* ImColorTextEdit_TextEditor(void);
+CIMGUI_API void ImColorTextEdit_destroy(TextEditor* self);
+
+CIMGUI_API void ImColorTextEdit_TextEditor_Render(TextEditor* self, const char* aTitle);
+CIMGUI_API void ImColorTextEdit_TextEditor_RenderSize(TextEditor* self, const char* aTitle, const ImVec2* aSize);
+CIMGUI_API void ImColorTextEdit_TextEditor_RenderSizeBorder(TextEditor* self, const char* aTitle, const ImVec2* aSize, bool aBorder);
+
+CIMGUI_API void ImColorTextEdit_TextEditor_SetText(TextEditor* self, const char *text);
+
+#endif //CIMGUI_COLOR_TEXT_EDIT_INCLUDED
